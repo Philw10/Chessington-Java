f1d52e1a275964cd08a07647319edca19d56338b
Phil Wilson <philipjwilson@gmail.com>
2021-12-13 14:13:53 +0000

    Red 5 complete

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -6,7 +6,6 @@ import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Pawn extends AbstractPiece {
@@ -25,22 +24,44 @@ public class Pawn extends AbstractPiece {
             moves.add(new Move(from, from.plus(2, 0)));
         }
 
-        if (colour == PlayerColour.WHITE & from.getRow() > 0){
+        if (colour == PlayerColour.WHITE & from.getRow() > 0 & from.getCol() < 7 & from.getCol() > 0){
             moves.add(new Move(from, from.plus(-1, 0)));
-
-        }else if (colour == PlayerColour.BLACK & from.getRow() < 7){
-            moves.add(new Move(from, from.plus(1, 0)));;
+            Piece checkToTakeOneWhite = board.get(new Coordinates(from.getRow()-1, from.getCol()-1));
+            if (checkToTakeOneWhite != null){
+                if (checkToTakeOneWhite.getColour().equals(PlayerColour.BLACK)) {
+                    moves.add(new Move(from, from.plus(-1, -1)));
+                }
+            }
+            Piece checkToTakeTwoWhite = board.get(new Coordinates(from.getRow()-1, from.getCol()+1));
+                if (checkToTakeTwoWhite != null) {
+                    if (checkToTakeTwoWhite.getColour().equals(PlayerColour.BLACK)) {
+                        moves.add(new Move(from, from.plus(-1, 1)));
+                    }
+                }
+        }else if (colour == PlayerColour.BLACK & from.getRow() < 7 & from.getCol() < 7 & from.getCol() > 0){
+            moves.add(new Move(from, from.plus(1, 0)));
+            Piece checkToTakeOneBlack = board.get(new Coordinates(from.getRow()+1, from.getCol()-1));
+            if (checkToTakeOneBlack != null) {
+                if (checkToTakeOneBlack.getColour().equals(PlayerColour.WHITE)) {
+                    moves.add(new Move(from, from.plus(1, -1)));
+                }
+            }
+            Piece checkToTakeTwoBlack = board.get(new Coordinates(from.getRow()+1, from.getCol()+1));
+            if (checkToTakeTwoBlack != null) {
+                if (checkToTakeTwoBlack.getColour().equals(PlayerColour.WHITE)) {
+                    moves.add(new Move(from, from.plus(1, 1)));
+                }
+            }
         }
 
         for (int i = 0; i < moves.size(); i++) {
             Piece piece = board.get(new Coordinates(moves.get(i).getTo().getRow(), moves.get(i).getTo().getCol()));
-            if (piece == null){
-                continue;
-            }else{
-                moves.remove(i);
+            if (from.getCol() == moves.get(i).getTo().getCol()) {
+                if (piece != null) {
+                    moves.remove(i);
+                }
             }
         }
-
         return moves;
     }
 }
